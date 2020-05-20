d49b6aadb130f381233378f920d1a20c0d707e85
Victor Giovannoni <vernalhav@usp.br>
2020-05-20 16:52:07 -0300

    Fixed compilation issues

--- client.c
+++ client.c
@@ -54,7 +54,7 @@ void *receive_messages(void *args){
 	while (strcmp(msg, QUIT_CMD)){
 
 		received_bytes = socket_receive(socket, buffer, WHOLE_MSG_LEN);
-		if (strlen(received_bytes == 0)) continue;	/* Possible transmission mistakes */
+		if (strlen(buffer) == 0) continue;	/* Possible transmission mistakes */
 
 		if (received_bytes <= 0){
 			console_log("receive_messages: Error receiving bytes!");
